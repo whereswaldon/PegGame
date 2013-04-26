0616a0a9dd97fcb1593893fa09a22c31c6a9d167
Christopher Waldon <wordsmith@embarqmail.com>
2013-04-26 11:50:28 -0400

    implemented State interface

--- src/PegGameState.java
+++ src/PegGameState.java
@@ -4,19 +4,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class PegGameState {
+public class PegGameState implements State{
 	private boolean[] isPeg;
-	private PegGraphL moves;
-	private final int BOARD_SIZE = 15;
+	private static PegGraphL moves;
+	private static final int BOARD_SIZE = 15;
 	
 	/**
 	 * Creates a new PegGameState with the data from the specified file
 	 * @param fileName
 	 */
-	public PegGameState(String fileName) {
+	public PegGameState() {
 		isPeg = new boolean[BOARD_SIZE];
 		for (int i = 0; i < BOARD_SIZE-1; i++)
 			isPeg[i] = true;
+	}
+	
+	public void setState(boolean[] pegs) {
+		isPeg = pegs;
+	}
+	
+	public boolean[] getState() {
+		return isPeg;
+	}
+	
+	public static void setMoves(String fileName) {
 		moves = new PegGraphL(BOARD_SIZE);
 		try {
 			Scanner file = new Scanner(new FileReader(fileName));
@@ -33,7 +44,21 @@ public class PegGameState {
 		return moves.toString() + "\n\n" + Arrays.toString(isPeg);
 	}
 	public static void main(String[] args) {
-		PegGameState state = new PegGameState("PegBoard.txt");
+		PegGameState.setMoves("PegBoard.txt");
+		PegGameState state = new PegGameState();
 		System.out.println(state);
+	}
+
+	@Override
+	public void copy(State anotherState) {
+		if (anotherState instanceof PegGameState) {
+			this.setState( ((PegGameState) anotherState).getState());
+		}
+	}
+	
+	public PegGameState clone() {
+		PegGameState copy = new PegGameState();
+		copy.setState(this.getState());
+		return copy;
 	}
 }
